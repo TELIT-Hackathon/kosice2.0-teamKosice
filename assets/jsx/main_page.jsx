3458ffc71f7a0d8f31551c3b62b8994755e8e44a
SerhiiTest <79904902+SerhiiTest@users.noreply.github.com>
2022-12-04 12:02:52 +0100

    Add files via upload

--- assets/jsx/main_page.jsx
+++ assets/jsx/main_page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function Element(props){
+    const [moreInfo,setMoreInfo] = React.useState(false);
     return <>
     <div className={"element-container"}>
         <img src={props.ImgUrl} className={"element-img"}></img>
@@ -9,7 +10,8 @@ function Element(props){
                 <span className={"element-title"}>{props.Title}</span>
                 <span className={"element-score"}>{props.Score}</span>
             </div>
-            <span className={"element-description"}>{props.Description}</span>
+            <span className={`element-description ${(moreInfo)?"active":""}`}>{props.Description}</span>
+            {/* <span className={"element-more"} onClick={(e)=>{setMoreInfo(!moreInfo)}}> {(moreInfo)?"↑":"↓"}</span> */}
         </div>
     </div>
     </>
@@ -21,10 +23,11 @@ function MainPage(props){
 
     const [data,setData] = React.useState([]);
     const [displayData,setDisplayData] = React.useState(false);
+    const [loadingData,setLoadingData] = React.useState(false);
 
 
     let postRequest = (description)=>{
-        console.log("clik");
+        setLoadingData(true);
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -49,8 +52,10 @@ function MainPage(props){
                 }else{
                     setDisplayData(false);
                 }
+                setLoadingData(false);
             })
             .catch(error => {
+                setLoadingData(false);
                 console.error('There was an error!', error);
                 setDisplayData(false);
             });
@@ -58,21 +63,51 @@ function MainPage(props){
 
     return <>
         <div className={"search-container"}>
-            <textarea onChange={(e)=>{setDesc(e.target.value)}}>
-
+            <div className={"presets-container"}>
+                <button className={"preset"} onClick={(e)=>{setDesc(`Overview The goal of this project is to create a social app that can be used for dating people. The app should provide users with an easy and intuitive way to find and connect with potential partners. 2. Scope The scope of this project includes the development of the following features: • User accounts: users should be able to create profiles with basic information, including age, gender, location, interests, and pictures. Users should also be able to search for other users based on these criteria. • Messaging: users should be able to send and receive messages to/from other users. • Matching: the app should have an algorithm that is able to match users based on their preferences. • Notifications: users should be able to receive notifications when they receive messages or when they are matched with someone. • Security: the app should have security measures in place to protect user data and ensure user privacy. 3. Design The app should have an attractive, modern design that is optimized for both desktop and mobile use. 4. Technology The app should be developed using the latest technologies, including HTML, CSS, JavaScript, React, and Node.js`)}}>Dating app</button>
+                <button className={"preset"} onClick={(e)=>{setDesc(`Project Requirements for Sports App (Coaching Portal) Objective: To develop a sports app to serve as a coaching portal for the sports environment, particularly Floorball, and to facilitate e-learning for its users. Scope: The scope of the project will include the development of a sports app that can be used for coaching and e-learning purposes. The app will be designed with the ability to facilitate Floorball-specific training and coaching activities, as well as to provide access to educational resources and content related to Floorball. Technologies: The project will need to make use of the following technologies: .NET, Big Data, AI, AR, IoT. Timeline: The timeline for the project is three months. Delivery: The project will be delivered as a Minimum Viable Product (MVP). Areas: Sports, Floorball, Coaching & learning.`)}}>Coaching portal</button>
+                <button className={"preset"} onClick={(e)=>{setDesc(`TakeAway Taxi is a Mobile application that provide services to
+passengers and taxi drivers .The TakeAway Taxi connects
+between passengers and taxi drivers using GPS system and
+enable passengers to order a taxi with their Smartphone .The
+application show to passengers nearby taxies to his location .The
+project has two parts the first one is an application run on
+mobile device and the second is a server on MySQL server.
+The importance of this application is managing passengers'
+booking in quick and easy way with shortest time possible
+without any need for third party to do this work. This project
+also allows passengers to stay in control with this real-time
+mobile application by tracking their rides on the way to their
+locations and by notifying drivers that there exists a request. The
+main objectives of this project are showing the nearby taxies to
+user location on map and enable user to order a taxi and
+tracking the arrival of the taxi.
+We will develop this application in order to work on mobile
+devices so we will mobile development language and for server
+part we will use PHP.`)}}>Taxi Mobile application</button>
+                {/* <button className={"preset"} onClick={(e)=>{setDesc(``)}}>4</button> */}
+            </div>
+            <textarea className={"search"} value={desc} onChange={(e)=>{setDesc(e.target.value)}} onKeyDown={(e)=>{if(e.keyCode == 13){console.log(e)}}}>
             </textarea>
-            <button onClick={(e)=>{postRequest(desc)}}>Match! :D</button>
+            <button className={"match"}  onClick={(e)=>{postRequest(desc)}}>Match :D</button>
         </div>
+        <hr className={"hr"}/>
         <div className={"data-container"}>
         {
-            (displayData)?
-            <>
+            (loadingData)?
+                <>
+                    <span className={"loading error"}>Loading Data</span> 
+                </>:<>
                 {
-                    data.map((e,id)=><Element Title={e.title} ImgUrl={e.image} Description={e.description} Score={e.score} key={id}/>)
-                }
-            </>:<>
-                <span className={"no-result"}>No result</span>
-            </>
+                (displayData)?
+                    <>
+                        {
+                            data.map((e,id)=><Element Title={e.title} ImgUrl={e.image} Description={e.description} Score={e.score} key={id}/>)
+                        }
+                    </>:<>
+                        <span className={"no-result error"}>No result</span>
+                    </>
+                }</>
         }
         </div>
     </>
